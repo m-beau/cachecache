a20d7ccd054925cc2db75c67d87bc3a782223371
m-beau <maximebeaujeanroch047@gmail.com>
2024-10-07 14:57:39 +0100

    Added multithread safety + persistency across python sessions for cached functions defined in notebook or console

--- cachecache/cachecache.py
+++ cachecache/cachecache.py
@@ -1,5 +1,7 @@
+import os
 from pathlib import Path
 import functools
+import threading
 from typing import Union, Optional
 
 from joblib import Memory
@@ -9,15 +11,71 @@ import inspect
 from cachecache.CONFIG import default_cache_path
 
 
+def make_arg_kwargs_dic(func, args, kwargs):
+    """
+    Make a dictionnary storing boths args and kwargs of func.
+
+    Adds args as a pair of key/values to the kwargs dictionnary:
+    - one pair where key = arg_name, and value = arg,
+    - one pair where key = arg_name + "_arg_index", and value = arg_index in args
+
+    Arguments:
+        - func: function
+        - args: list of arguments from func
+        - kwargs: dict of keyword arguments from func
+
+    Returns:
+        - args_kwargs: dictionnary holding boths args and kwargs.
+    """
+
+    # Get function signature
+    sig = inspect.signature(func)
+
+    # Identify all positional and keyword arguments
+    arg_kwarg_names = [param.name for param in sig.parameters.values()]
+    wrong_kwargs = [name for name in kwargs.keys() if name not in arg_kwarg_names]
+    assert (
+        len(wrong_kwargs) == 0
+    ), f"{func.__name__}() got >=1 unexpected keyword argument(s): {wrong_kwargs}"
+
+    # Add arguments PASSED as positional arguments, in args
+    # (they can be defined either as positional or keyword arguments!)
+    args_kwargs = {}
+    for i, value in enumerate(args):
+        name = arg_kwarg_names[i]
+        args_kwargs[name] = value
+        args_kwargs[name + "_arg_index"] = i
+
+    # Add arguments PASSED as keyword arguments, in kwargs
+    # (they can be defined either as positional or keyword arguments!)
+    args_kwargs = {**args_kwargs, **kwargs}
+
+    # Add keyword arguments NOT PASSED, with default values
+    for name, param in sig.parameters.items():
+        if (name not in args_kwargs) and \
+           (param.default is not inspect.Parameter.empty):
+            args_kwargs[name] = param.default
+
+    return args_kwargs
+
+
 class Cacher:
     """
     Class embedding a decorator to cache any function at 'cache_path' ("~/.cachecache" by default).
+    
     The decorated function can alter the caching behaviour at run time with the following arguments:
         - again: bool, whether to recompute and overwrite the cached results
              (if False, loads from cache if found in cache)
         - cache_results: bool, whether to cache the results
                          (if False, does not attempt to load form cache either)
         - cache_path: None|str, set alternative cache directory at run time
+
+    *** Arguments ***
+        - cache_path: directory to cache the results of functions decorated with cacher = Cacher().
+        - caching_memory_allocation: int, max size of cache in bytes.
+
+    *** Returns ***
+        - cacher: the caching decorator.
 
     *** Usage ***
 
@@ -79,14 +137,6 @@ class Cacher:
     so it is better practice to instanciate a single cacher to use across functions
     rather than resorting to using the 'cache_path' argument at run time,
     especially for functions that are intented to be called many times.
-
-
-    *** Arguments ***
-        - cache_path: directory to cache the results of functions decorated with cacher = Cacher().
-        - caching_memory_allocation: int, max size of cache in bytes.
-
-    *** Returns ***
-        - cacher: the caching decorator.
     """
 
     def __init__(
@@ -94,10 +144,11 @@ class Cacher:
         cache_path: Union[str, Path] = default_cache_path,
         caching_memory_allocation: Union[int, None] = None,
     ):
-        self.global_cache_memory = instanciate_joblib_cache(
+        self.global_cache_memory = self.instanciate_joblib_cache(
             cache_path, caching_memory_allocation
         )
         self.input_caching_memory_allocation = caching_memory_allocation
+        self._lock = threading.Lock()
 
     def __repr__(self):
         path = self.global_cache_memory.__repr__().split("=")[-1][:-1]
@@ -109,6 +160,7 @@ class Cacher:
         )
 
     def __call__(self, func):
+        "Calling cacher returns the decorated (cached) function."
         return self._decorator(func)
 
     def _decorator(self, func_to_cache):
@@ -126,6 +178,11 @@ class Cacher:
         """
         assert callable(func_to_cache), f"{func_to_cache} is not callable!"
 
+        # Guarantees persistent cache directory across ipython sessions
+        # for functions defined inside ipython sessions (e.g. jupyter notebook)
+        if '__main__' in func_to_cache.__module__:
+            func_to_cache.__module__ = 'cachecache_persistent'
+
         @functools.wraps(func_to_cache)
         def cached_func(*args, **kwargs):
 
@@ -138,83 +195,110 @@ class Cacher:
             if not cache_results:
                 return func_to_cache(*args, **kwargs)
 
-            # Define cache, global or custom
-            if cache_path is None:
-                cache_memory = self.global_cache_memory
-            else:
-                # no way to customize the allocated cache memory for cache initialized at function run time
-                cache_memory = instanciate_joblib_cache(
-                    cache_path, caching_memory_allocation=None
+            # Ensure thread-safe cache operations
+            with self._lock:
+                # Define cache, global or custom
+                if cache_path is None:
+                    cache_memory = self.global_cache_memory
+                else:
+                    # no way to customize the allocated cache memory for cache initialized at function run time
+                    cache_memory = self.instanciate_joblib_cache(
+                        cache_path, caching_memory_allocation=None
+                    )
+                
+                # If path not writable, cache_memory will be None
+                # so return the function unaltered
+                if cache_memory is None:
+                    return func_to_cache(*args, **kwargs)
+
+                # Cache function, ignoring arguments that alter caching behavior
+                # only if they exist in the function signature
+                arg_kwargs = make_arg_kwargs_dic(func_to_cache, args, kwargs)
+                arguments_to_ignore = [
+                    k for k in ["again", "cache_results", "cache_path"] if k in arg_kwargs
+                ]
+                func_to_cache_cached = cache_memory.cache(
+                    func_to_cache, ignore=arguments_to_ignore
                 )
 
-            # Cache function, ignoring arguments that alter caching behavior
-            # only if they exist in the function signature
-            arg_kwargs = make_arg_kwargs_dic(func_to_cache, args, kwargs)
-            arguments_to_ignore = [
-                k for k in ["again", "cache_results", "cache_path"] if k in arg_kwargs
-            ]
-            func_to_cache_cached = cache_memory.cache(
-                func_to_cache, ignore=arguments_to_ignore
-            )
+                # Reload or recompute results
+                mem = func_to_cache_cached.call_and_shelve(*args, **kwargs)
 
-            # Reload or recompute results
-            mem = func_to_cache_cached.call_and_shelve(*args, **kwargs)
+                # If again is True, clear the cache to enforce recomputing the results
+                if again:
+                    mem.clear()
 
-            # If again is True, clear the cache to enforce recomputing the results
-            if again:
-                mem.clear()
-
-            # Reload results (or recompute them if again was True)
-            mem = func_to_cache_cached.call_and_shelve(*args, **kwargs)
-            results = mem.get()
+                # Reload results (or recompute them if again was True)
+                mem = func_to_cache_cached.call_and_shelve(*args, **kwargs)
+                results = mem.get()
 
             return results
 
         return cached_func
 
+    def is_writable(self,
+                    path: Union[str, Path]) -> bool:
+        """Check if the given path is writable without creating it."""
+        if isinstance(path, str):
+                path = Path(path)
+        if path.exists():
+            return os.access(path, os.W_OK)
+        elif path.parent.exists():
+            return os.access(path.parent, os.W_OK)
+        else:
+            print("WARNING {path.parent} does not exist - caching aborted.")
+            return False
 
-def instanciate_joblib_cache(
-    path: int, caching_memory_allocation: Union[int, None] = None
-):
-    """
-    Initialize joblib cache Memory object at 'path',
-    with a size limit of caching_memory_allocation bytes.
-    - path: str, path to cachinf directory
-    - caching_memory_allocation: int, allocated memory for cache in bytes
-    """
-    # Format and process path
-    path = Path(path).expanduser()
-    if not path.parent.exists():
-        error = (
-            f"WARNING you attempted to cache your results at {str(path)}, "
-            "but the parent directory doesn't exist! Change your target cache, "
-            f"or create {str(path.parent)}."
-        )
-        raise ValueError(error)
-    path.mkdir(exist_ok=True)
+    def instanciate_joblib_cache(
+        self,
+        path: int,
+        caching_memory_allocation: Union[int, None] = None
+    ):
+        """
+        Initialize joblib cache Memory object at 'path',
+        with a size limit of caching_memory_allocation bytes.
+        - path: str, path to cachinf directory
+        - caching_memory_allocation: int, allocated memory for cache in bytes
 
-    # Instanciate joblib memory object
-    memory = Memory(path, verbose=0)
-
-    # Limit cache size
-    free_memory_bytes = psutil.disk_usage(str(path)).free
-    if caching_memory_allocation is None:
-        caching_memory_allocation = int(free_memory_bytes - 1e9)
-
-    if free_memory_bytes * 1e-9 < 5:
-        print(
-            (
-                f"WARNING less than 5GB free at {str(path)} - "
-                "caching will quickly fill up the remaining space "
-                "(allowed all available space minus 1GB "
-                f"(i.e. {caching_memory_allocation*1e-9}GB))."
+        If 'path' is not writable: returns None
+        """
+        # Format and process path
+        path = Path(path).expanduser()
+        if not path.parent.exists():
+            error = (
+                f"WARNING you attempted to cache your results at {str(path)}, "
+                "but the parent directory doesn't exist! Change your target cache, "
+                f"or create {str(path.parent)}."
             )
-        )
+            raise ValueError(error)
+        
+        if not self.is_writable(path):
+            return None
 
-    memory.caching_memory_allocation = caching_memory_allocation
-    memory.reduce_size(bytes_limit=caching_memory_allocation)
+        path.mkdir(exist_ok=True)
 
-    return memory
+        # Instanciate joblib memory object
+        memory = Memory(path, verbose=0)
+
+        # Limit cache size
+        free_memory_bytes = psutil.disk_usage(str(path)).free
+        if caching_memory_allocation is None:
+            caching_memory_allocation = int(free_memory_bytes - 1e9)
+
+        if free_memory_bytes * 1e-9 < 5:
+            print(
+                (
+                    f"WARNING less than 5GB free at {str(path)} - "
+                    "caching will quickly fill up the remaining space "
+                    "(allowed all available space minus 1GB "
+                    f"(i.e. {caching_memory_allocation*1e-9}GB))."
+                )
+            )
+
+        memory.caching_memory_allocation = caching_memory_allocation
+        memory.reduce_size(bytes_limit=caching_memory_allocation)
+
+        return memory
 
 
 # cachecache default global cache
@@ -302,51 +386,3 @@ def distributed_cacher(
         return locally_cached_func
 
     return decorator
-
-
-def make_arg_kwargs_dic(func, args, kwargs):
-    """
-    Make a dictionnary holding boths args and kwargs.
-
-    Adds args as a pair of key/values to the kwargs dictionnary:
-    - one pair where key = arg_name, and value = arg,
-    - one pair where key = arg_name + "_arg_index", and value = arg_index in args
-
-    Arguments:
-        - func: function
-        - args: list of arguments from func
-        - kwargs: dict of keyword arguments from func
-
-    Returns:
-        - args_kwargs: dictionnary holding boths args and kwargs.
-    """
-
-    # Get function signature
-    sig = inspect.signature(func)
-
-    # Identify all positional and keyword arguments
-    arg_kwarg_names = [param.name for param in sig.parameters.values()]
-    wrong_kwargs = [name for name in kwargs.keys() if name not in arg_kwarg_names]
-    assert (
-        len(wrong_kwargs) == 0
-    ), f"{func.__name__}() got >=1 unexpected keyword argument(s): {wrong_kwargs}"
-
-    # Add arguments PASSED as positional arguments, in args
-    # (they can be defined either as positional or keyword arguments!)
-    args_kwargs = {}
-    for i, value in enumerate(args):
-        name = arg_kwarg_names[i]
-        args_kwargs[name] = value
-        args_kwargs[name + "_arg_index"] = i
-
-    # Add arguments PASSED as keyword arguments, in kwargs
-    # (they can be defined either as positional or keyword arguments!)
-    args_kwargs = {**args_kwargs, **kwargs}
-
-    # Add keyword arguments NOT PASSED, with default values
-    for name, param in sig.parameters.items():
-        if (name not in args_kwargs) and \
-           (param.default is not inspect.Parameter.empty):
-            args_kwargs[name] = param.default
-
-    return args_kwargs
